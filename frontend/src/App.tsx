216ddaa6239f8de5c5b1428f7e6653b966d46655
Mirka Lison <mirka.lison@gmail.com>
2020-05-26 21:23:14 +0300

    removed unused use subscription

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 // import Chart from './components/Chart'
-import { useQuery, useMutation, useSubscription } from "@apollo/react-hooks";
+import { useQuery, useMutation } from "@apollo/react-hooks";
 import { TIMESTAMPS, IGNORE_EVENTS_MUTATION, IGNORE_EVENTS_QUERY, TIMESTAMPS_SUBSCRIPTION } from './api'
 import ErrorMessage from './components/Error'
 import styles from './style/Button.module.scss'
@@ -12,9 +12,6 @@ const App = () => {
   const { subscribeToMore, loading, error, data } = useQuery(TIMESTAMPS, {
     variables: {day: today}
   })
-  const { data: ineData } = useSubscription(
-    TIMESTAMPS_SUBSCRIPTION
-  );
 
   const { error: ignoreError, data: ignoreEventData } = useQuery(IGNORE_EVENTS_QUERY)
   const [ignoreEvents] = useMutation(IGNORE_EVENTS_MUTATION)
