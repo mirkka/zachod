4fad6cda20d576acc9633a4dc7e331a887a78abd
Mirka Lison <mirka.lison@gmail.com>
2020-10-17 22:12:24 +0300

    updated device code

--- backend/index.js
+++ backend/index.js
@@ -6,18 +6,11 @@ const debounce = require('lodash/debounce');
 // You can use any gpio pin
 // install npm button library first
 const Gpio = require('onoff').Gpio,
-button = new Gpio(7, 'in', 'both');
+button = new Gpio(21, 'in', 'both');
 
 const debounceTime = 1000 * 60;
 
 const myThingName = 'sdk-nodejs-e2f7328d-663e-42d9-bf84-a567b35d3841';
-
-mythingstate = {
-  "state": {
-    "movement": 0,
-    "timestamp": 0
-  }
-}
 
 var device = awsIot.device({
   keyPath: './pir_2.private.key',
@@ -29,18 +22,24 @@ var device = awsIot.device({
   debug: true
 });
 
-const callback = debounce((err, value) => {
-  console.log("Movement detected ")
-  delete mythingstate['version']; //Cleanup to post to AWS
-  mythingstate["state"]["movement"] = value
-  mythingstate["state"]["timestamp"] = Date.now()
-  device.publish('topic_2', JSON.stringify(mythingstate));
-  console.log("Update:" + mythingstate.state.movement);
+const handleEvent = debounce(()=> {
+  const state = {
+    "state": {
+      "movement": 1,
+      "timestamp": Date.now()
+    }
+  }
+  device.publish('topic_2', JSON.stringify(state));
+  console.log(`${new Date().toISOString()} Publishing event`)
 }, debounceTime);
+
+const callback = (err, value) => {
+  console.log(`${new Date().toISOString()} Movement detected: ${value}`)
+  if(value === 1) handleEvent()
+};
 
 device
  .on('connect', function() {
-  console.log('connect');
+  console.log(`${new Date().toISOString()} Connect`);
   button.watch(callback);
  });
-
